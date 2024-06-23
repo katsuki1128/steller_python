55bcb5a46cd39daada6497c03f8e6688d62c0069
katsuki1128 <katsuki1128@gmail.com>
2024-06-23 19:46:34 +0900

    image display

--- app.py
+++ app.py
@@ -15,12 +15,9 @@ matplotlib.use("Agg")
 app = Flask(__name__)
 
 # ======================
-# CDESのバックエンドと接続
+# CDESのバックエンドと接続とOIDC認証
 # ======================
 connection = openeo.connect("openeo.dataspace.copernicus.eu")
-# 接続の確認のため、SENTINEL2_L2のメタデータ詳細を取得し出力する
-collection_metadata = connection.describe_collection("SENTINEL2_L2A")
-# print(collection_metadata)
 connection.authenticate_oidc()
 
 
@@ -51,8 +48,6 @@ def process():
         max_cloud_cover=20,
     )
 
-    # ファイル名を指定
-    # filename = "new.nc"
     # 現在の日時を取得してファイル名を生成
     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"s2_{current_time}.nc"
@@ -67,30 +62,9 @@ def process():
 
     # 保存したncデータをxarrayとして読み込む
     ds = xarray.open_dataset(filename)
-    # print(ds)
-
-    # 雲のカバー率を計算
-    # cloud_cover = (ds["SCL"] == 3).mean(dim=["x", "y"])
-
-    # 雲のカバー率が最も少ないタイムステップを選択
-    # best_time_step = cloud_cover.argmin().item()
-
-    # Scene Classification Layer (SCL)を取得した時系列で描画
-    # ds["SCL"].plot.imshow(col="t")
-    # plt.show()
-
-    # xarrayデータセットを新しいデータアレイへ変換 (bands, t, x, y)
-    # 新しい次元名をbandsとする（デフォルト名はvariable）
-    # トゥルーカラー画像を生成 (最初のタイムステップを使用)
-    # rgb_data = ds[["B04", "B03", "B02"]].isel(t=best_time_step).to_array(dim="bands")
-
     rgb_data = ds[["B04", "B03", "B02"]].isel(t=0).to_array(dim="bands")
 
-    # デバッグ: rgb_dataの次元と座標を確認
-    # print(rgb_data)
-
     output_image = f"static/rgb_{current_time}.png"
-    # print(rgb_data)
 
     # staticディレクトリが存在しない場合は作成
     if not os.path.exists("static"):
@@ -101,25 +75,20 @@ def process():
     ax.imshow(rgb_data.transpose("y", "x", "bands").values / 10000.0)  # 正規化して表示
     plt.axis("off")
     plt.savefig(output_image, bbox_inches="tight", pad_inches=0)
-
-    # # # 2024年2月10日から2024年3月21日までのデータをトゥルーカラー画像で描画
-    # rgb_data.plot.imshow(vmin=0, vmax=2000, col="t", col_wrap=3, robust=True)
-    # plt.show()
-    # plt.savefig(output_image)
     plt.close(fig)
 
     # 使用した.ncファイルを削除
     os.remove(filename)
 
-    # return render_template("index.html", image_url=output_image)
-    # 画像のパスを返す
-    return render_template("index.html", image_url=output_image, filename=filename)
-    # return "プロセスが完了しました。トゥルーカラー画像が生成されました。"
-
-
-# rds = rioxarray.open_rasterio("s2-amami.nc", decode_times=False, masked=True)
-# rds["t"] = ds["t"]  # 時間データの上書き
-# rds.isel(t=-1).rio.to_raster("sample_time_slice.tif")
+    # 画像のパスと地理的境界を返す
+    return render_template(
+        "index.html",
+        image_url=output_image,
+        west=west,
+        south=south,
+        east=east,
+        north=north,
+    )
 
 
 if __name__ == "__main__":
